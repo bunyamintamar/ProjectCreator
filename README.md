28d2ca540e89f7d2d3e6370681a759f44e5e9d0f
Bünyamin TAMAR <51991669+bunyamintamar@users.noreply.github.com>
2022-02-14 00:26:45 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# PROJECT CREATOR
+You can create a **hello world** and a **unit test** project  
+
+`createTest` creates a unit test project  
+--> CMakelists.txt  
+--> test.cpp  
+--> build_test.sh  
+
+`createCppProject` creates a hello world project  
+--> CMakelists.txt  
+--> hello.cpp  
+--> build_hello.sh  
+
+* * *
+
+# HOW TO INSTALL?
+write command below in terminal  
+```bash
+git clone https://github.com/bunyamintamar/ProjectCreator.git
+cd ProjectCreator/
+chmod +x install_projectCreator.sh
+./install_projectCreator.sh
+```
---
+++ install_projectCreator.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo  '--------------------------------------------------------------------'
+echo  'WELLCOME TO INSTALLER FOR PROJECT CREATOR'
+echo  '--------------------------------------------------------------------'
+echo
+echo  'createTest and createCppProject will be installed'
+
+sudo  apt --fix-broken install
+sudo  apt-get update -y
+sudo  apt-get install build-essential -y
+sudo  apt-get remove cmake -y
+sudo  apt-get install cmake -y
+echo  '--------------------------------------------------------------------'
+echo  'CMAKE reinstalled'
+echo  '--------------------------------------------------------------------'
+echo
+
+chmod +x createCppProject
+chmod +x createTest
+
+sudo  cp createCppProject /usr/bin/
+sudo  cp createTest       /usr/bin/
+echo  '--------------------------------------------------------------------'
+echo  'createCppProject and createTest is copied to /usr/bin'
+echo  '--------------------------------------------------------------------'
+echo
+echo  '--------------------------------------------------------------------'
+echo  '|  written by BUNYAMIN TAMAR                                       |'
+echo  '|  Mail     : bunyamintamar@yandex.com                             |'
+echo  '|  GitHub   : https://github.com/bunyamintamar/ProjectCreator.git  |'
+echo  '|  Linkedin : https://www.linkedin.com/in/bunyamintamar            |'
+echo  '--------------------------------------------------------------------'
+echo  'good luck!'
+echo
